852911a5fdf9aa6f896d23a991c07e55e7ccbb94
snziv gupta <snzivgupta@live.com>
2021-05-09 17:42:03 +0530

    local storage cahpter added with example

---
+++ 01Advance/localStorage.js
@@ -0,0 +1,18 @@
+//basic persistance using storage on clientside
+
+//common storage functions that implements storage interface
+// setItem
+// getItem
+// removeItem
+// clear
+// key
+// length
+
+
+localStorage.setItem('firstName','John');
+localStorage.setItem('lastName','Doe');
+
+sessionStorage.setItem('firstName','John');
+localStorage.removeItem('firstName');
+
+
